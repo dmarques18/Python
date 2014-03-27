d598db1cf8c2088b8d5dc6ea6c85267e67d14809
dmarques <dmarques@nyc1alggce4997.ggg.grey.global>
2014-03-27 14:31:52 -0400

    infinite loop and break

--- src/higher level.py
+++ src/higher level.py
@@ -5,12 +5,19 @@ if __name__ == "__main__":
     print "Hello World";
     
 b=1
-
 while b<=10:
     print b
     b+=1
     
 list = ['soccer', 'football', 'hockey']
-
 for food in list:
     print 'I play ' + food
+
+ages = {'dad':40, 'mom':38, 'sister':20}
+for item in ages:
+    print item,' is ',ages[item],' years old.'
+
+#infinite loop and break
+while 1:
+    name = raw_input('Enter name: ')
+    if name == 'quit': break
